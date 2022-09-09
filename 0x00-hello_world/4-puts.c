1528acc482eb5a15afd6cee83b2ba84dc1094f92
jossyfresh <111150600+jossyfresh@users.noreply.github.com>
2022-09-08 17:43:54 -0700

    Delete 4-puts.c

--- 0x00-hello_world/4-puts.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/**
- * main function - prints the phrase "Programming is like building a multilingual puzzle"
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
