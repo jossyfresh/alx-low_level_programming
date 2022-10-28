487667d03b093553135fcabd80c0dafbe2cc307d
jossyfresh <111150600+jossyfresh@users.noreply.github.com>
2022-10-28 13:31:52 -0700

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,42 +1,34 @@
 #include "main.h"
+
 /**
- * binary_to_uint - function that converts a binary number to an
- * unsigned int
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * @b: Pointer with the string whit the number to convert
- *
- * Return: The number converted
+ * Return: the converted number
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int sum = 0;
-	int i, x, n;
+	unsigned int total = 0;
+	int len, i;
+	int decval = 1;
 
-	if (b == NULL)
-	{
+	if (!b)
 		return (0);
-	}
 
-	n = strlen(b) - 1;
+	for (len = 0; b[len] != '\0'; len++)
+		;
 
-	/* Realizamos la operacion para hallar suma */
-	for (x = 0, i = 0; n >= x; n--)
+	i = (len - 1);
+	for (; i >= 0; i--)
 	{
-		switch (b[i])
-		{
-			case '1':
-				sum = sum + _pow(2, n);
-				i++;
-				break;
-			case '0':
-				sum = sum + 0;
-				i++;
-				break;
-			default:
-				return (0);
-		}
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		if (b[i] == '1')
+			total += decval;
+		decval *= 2;
 	}
 
-	return (sum);
+	return (total);
 }
